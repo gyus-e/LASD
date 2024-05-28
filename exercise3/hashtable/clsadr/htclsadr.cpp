df2a79a24523813eb73fa9eb48968f82d7f7cf1c
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-05-28 20:40:08 +0200

    verifica dimensione massima in resize

--- exercise3/hashtable/clsadr/htclsadr.cpp
+++ exercise3/hashtable/clsadr/htclsadr.cpp
@@ -210,7 +210,9 @@ void HashTableClsAdr<Data>::Resize(unsigned long newSize)
     newSize = pow(2, exp);
     //Verifica che la dimensione minima sia rispettata
     newSize = std::max(newSize, INITIAL_SIZE);
-
+    //Verifica che la dimensione massima sia rispettata
+    newSize = std::min(newSize, MAX_SIZE);
+    
     //Verifica che la resize vada effettuata davvero
     if (newSize == this->tableSize)
     {
