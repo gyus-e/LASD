55e97ed4161889530337fc1b1cca6deef9a9ae7e
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-05-29 14:59:25 +0200

    fixed coprimeFun to always produce uneven number

--- exercise3/hashtable/opnadr/htopnadr.cpp
+++ exercise3/hashtable/opnadr/htopnadr.cpp
@@ -362,14 +362,14 @@ void HashTableOpnAdr<Data>::Clear()
 template <typename Data>
 unsigned long HashTableOpnAdr<Data>::HashKey(const Data & dat, const unsigned long i) const
 {
-    return (this->HashKey(dat) + ((i * this->coprimeFun(dat)) % this->prime)) % this->tableSize;
+    return (this->HashKey(dat) + (i * this->coprimeFun(dat))) % this->tableSize;
 }
 
 //Deve produrre sempre numeri dispari (coprimi con tableSize, che è una potenza di 2)
 template <typename Data>
 unsigned long HashTableOpnAdr<Data>::coprimeFun (const Data & dat) const 
 {
-    return ( (( (this->enchash(dat) % this->prime) * 2 ) % this->prime) + 1) % this->prime;
+    return ( ( (this->acoeff + this->bcoeff) % this->prime ) * 2 + 1); 
 }
 
 template <typename Data>
