af58f2a2732a099bf132a6d0f450de1aafdfe4e4
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-05-29 22:14:38 +0200

    don't specify number of threads

--- exercise3/valgrind.sh
+++ exercise3/valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make -f makefile.valgrind -j6
+make -f makefile.valgrind -j
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --show-error-list=yes --verbose --log-file=valgrind.log ./main > output.txt
 cat valgrind.log
