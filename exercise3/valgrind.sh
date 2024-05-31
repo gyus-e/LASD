c161a968d743ac228eddb6105ab6d2104ffd0bc9
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-05-31 21:43:11 +0200

    now using g++-13

--- exercise3/valgrind.sh
+++ exercise3/valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make -f makefile.valgrind -j
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --show-error-list=yes --verbose --log-file=valgrind.log ./main > output.txt
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --show-error-list=yes --verbose --log-file=valgrind.log ./main > out.txt
 cat valgrind.log
