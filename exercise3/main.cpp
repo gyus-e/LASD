66c474e185681677f9c0490645a2c7f6ba5a43e7
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-10-12 08:47:03 +0200

    switch between lasdtest and mytest

--- exercise3/main.cpp
+++ exercise3/main.cpp
@@ -11,7 +11,22 @@
 
 int main() {
   std::cout << "Lasd Libraries 2024" << std::endl;
-  mytest();
-  lasdtest();
+  
+  int i = 0;
+  std::cout << "Press 1 for lasdtest()\nPress 2 for mytest()\n";
+  std::cin >> i;
+  switch (i)
+  {
+    case 1:
+    lasdtest();
+    break;
+
+    case 2:
+    mytest();
+    break;
+
+    default:
+    break;
+  }
   return 0;
 }
